3bc6b34eaa58f782bf5440669ce8eeb2a929747f
Pauline Leininger <pauline.leininger@gmail.com>
2020-12-13 14:37:57 +0100

    Added waiting room

---
+++ CaboClient/app/src/main/java/msp/group3/caboclient/WaitingRoomActivity.java
@@ -0,0 +1,18 @@
+package msp.group3.caboclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class WaitingRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting_room);
+    }
+
+    public void sendMessage(View view) {
+    }
+}
