83928a1f985d5e969c77d71de458de40ba148b6a
Pauline Leininger <pauline.leininger@gmail.com>
2021-01-25 01:44:19 +0100

    tried (unsuccessfully) to add settings fragment
    tried (unsuccessfully) to add settings fragment

--- CaboClient/app/src/main/java/msp/group3/caboclient/InGameActivity.java
+++ CaboClient/app/src/main/java/msp/group3/caboclient/InGameActivity.java
@@ -2,6 +2,8 @@ package msp.group3.caboclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentTransaction;
 
 
 import android.animation.Animator;
@@ -48,6 +50,8 @@ import static msp.group3.caboclient.TypeDefs.*;
  * this is an example for a zoomable and scrollable layout
  */
 public class InGameActivity extends AppCompatActivity implements Communicator.CommunicatorCallback {
+    private static final String TAG_CHAT = "chat_fragment";
+    private static final String TAG_SETTINGS = "settings_fragment";
     protected WebSocketClient webSocketClient;
     private Communicator communicator;
 
@@ -135,6 +139,7 @@ public class InGameActivity extends AppCompatActivity implements Communicator.Co
 
     private boolean initialRound = true;
     private Player caboplayer = null;
+    private androidx.fragment.app.FragmentContainerView settingsFragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -209,18 +214,24 @@ public class InGameActivity extends AppCompatActivity implements Communicator.Co
 
         pickCardsStackButton.setEnabled(false);
 
-        //TODO: Chat fragment integration
         if (savedInstanceState == null) {
             Bundle bundle = new Bundle();
             bundle.putInt("some_int", 0);
             getSupportFragmentManager().beginTransaction()
                     .setReorderingAllowed(true)
                     .add(R.id.fragment_chat, InGameChatFragment.class, bundle)
+                    //.add(R.id.fragment_settings, SettingsFragment.class, bundle)
+                    //.add(R.id.fragment_chat, new InGameChatFragment(), TAG_CHAT)
+                    //.add(R.id.fragment_settings, new SettingsFragment(), TAG_SETTINGS)
                     .commit();
         }
 
         chatFragmentContainer = findViewById(R.id.fragment_chat);
         chatFragmentContainer.setVisibility(View.INVISIBLE);
+
+        //settingsFragmentContainer = findViewById(R.id.fragment_settings);
+
+        //switchToSettingsFragment();
 
 
         communicator = Communicator.getInstance(this);
@@ -237,6 +248,27 @@ public class InGameActivity extends AppCompatActivity implements Communicator.Co
 
     }
 
+    private void switchToSettingsFragment() {
+        SettingsFragment fragA = (SettingsFragment) getSupportFragmentManager().findFragmentByTag(TAG_SETTINGS);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.detach(getSupportFragmentManager().findFragmentByTag(TAG_CHAT));
+        fragmentTransaction.attach(fragA);
+        fragmentTransaction.addToBackStack(null);
+
+        fragmentTransaction.commitAllowingStateLoss();
+        getSupportFragmentManager().executePendingTransactions();
+    }
+
+    private void switchToChatFragment() {
+        InGameChatFragment fragA = (InGameChatFragment) getSupportFragmentManager().findFragmentByTag(TAG_CHAT);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.detach(getSupportFragmentManager().findFragmentByTag(TAG_CHAT));
+        fragmentTransaction.attach(fragA);
+        fragmentTransaction.addToBackStack(null);
+
+        fragmentTransaction.commitAllowingStateLoss();
+        getSupportFragmentManager().executePendingTransactions();
+    }
 
     private void setUpOnClickListeners() {
 
