ddbd446da4840ed3fa30b2d5344e2e3677956a0c
Qi.Liu <Q.liu@campus.lmu.de>
2021-02-07 17:09:03 +0100

    Add rule introduction

--- CaboClient/app/src/main/java/msp/group3/caboclient/CaboView/InGameActivity.java
+++ CaboClient/app/src/main/java/msp/group3/caboclient/CaboView/InGameActivity.java
@@ -6,6 +6,7 @@ import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Point;
@@ -31,6 +32,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import msp.group3.caboclient.CaboController.BackgroundSoundService;
@@ -161,6 +163,11 @@ public class InGameActivity extends AppCompatActivity implements Communicator.Co
     private boolean initialRound = true;
     private Player caboplayer = null;
     private MediaPlayer soundPlayer;
+
+
+
+    private AlertDialog.Builder builder = null;
+    private AlertDialog ruleDialog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -299,12 +306,54 @@ public class InGameActivity extends AppCompatActivity implements Communicator.Co
     }
 
     private void setUpOnClickListeners() {
+        String msg = "Cabo is a round-based Online-Multiplayer Cardgame for up to 4 Players." +
+                "\nThe Game:" +
+                "\nEvery player has 4 cards. The goal is to collect as few points as possible by swapping drawn cards with your own, or by using action-cards, to swap cards with the other players." +"\nRules:" +
+                "\nAt the beginning of a round, every player can look at and memorize 2 cards. When it's your turn, you can draw a card. There are normal cards and action cards." +
+                "\nNormal Cards:" +
+                "\nCards of the values 2 - 6 are normal cards. You can use these only to swap with your cards or simply discard them." +
+                "\nAction Cards:" +
+                "\nPeek: 7+8 Allow you to take a look at one of your cards" +
+                "\nSpy: 9+10 Allow you to take a look at any enemy card" +
+                "\nSwap: J+Q Allow you to swap any 2 cards on the field" +
+                "\nCard Values:" +
+                "\nEvery Card with a number is worth its indicated number." +
+                "\nJack is worth 11 Points, Queen is worth 12 Points and the King is worth 13 Points" +
+                "\nAce is worth 0 Points and the Joker is worth -1 Point" +
+                "\nRounds:" +
+                "\nIf you think, your points are low enough, you can call CABO!" +
+                "\nThis will end your turn, and everyone else has 1 last turn." +
+                "\nAfterwards the points of every player are summed up and a new round begins." +
+                "\nWinning:" +
+                "\nBy default the game ends as soon as the first player has reached 100 points. The player with the lowest score at this point wins." +
+                "\nThe max score can also be adjusted in the settings." +
+                "\nGameplay:" +
+                "\nTo interact in the game, you can click cards, swipe or pinch zoom the table to move around, chat with other players, or show your emotions by changing your emoji.";
+
+        builder = new AlertDialog.Builder(this);
+        builder
+                .setTitle("Game Rule")
+                .setMessage(msg)
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                        dialog.cancel();
+                    }
+                })
+                ;
+        ruleDialog = builder.create();
+        ruleDialog.getWindow().setBackgroundDrawableResource(R.color.beige);
+
 
         questionBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 playSound(R.raw.select_sound);
                 //TODO show game rules
+                Log.d(TAG_SETTINGS, "Click Question Button");
+                ruleDialog.show();
             }
         });
 
