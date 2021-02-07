c2df67c24efeaf64a5bcdce013fdb3001234a432
sophiasigethy <sophia@sigethy.de>
2021-02-07 18:08:49 +0100

    bug fix in KI when swapping cards

--- CaboClient/app/src/main/java/msp/group3/caboclient/CaboController/TypeDefs.java
+++ CaboClient/app/src/main/java/msp/group3/caboclient/CaboController/TypeDefs.java
@@ -12,9 +12,9 @@ public class TypeDefs {
     public static String playing =  "playing";
     public static String welcomeMessage =  "Hello";
 
-    public static String URI ="ws://141.84.213.238:8080/chat"; //server
+    //public static String URI ="ws://141.84.213.238:8080/chat"; //server
     //public static String URI ="ws://192.168.178.31:8080/chat"; //georg
-    //public static String URI ="ws://192.168.178.27:8080/chat"; //sophia
+    public static String URI ="ws://192.168.178.27:8080/chat"; //sophia
     //public static String URI ="ws://192.168.2.125:8080/chat"; //pauline
 
 
